7295683043d8989d6528dc9fcb580e6e3a52ceb4
theflanders2 <kenneth.flanders.ii@gmail.com>
2023-11-25 09:50:46 +0100

    update comments in app.js

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+// Home route
 app.get('/', (req, res) => {
     console.log(__dirname);
     res.sendFile(`${__dirname}/views/index.html`)
 
 })
 
+// About route
 app.get('/about', (req, res) => {
     console.log(__dirname);
     res.sendFile(`${__dirname}/views/about.html`)
 
 })
 
+// Works route
 app.get('/works', (req, res) => {
     console.log(__dirname);
     res.sendFile(`${__dirname}/views/works.html`)
 
 })
 
+// Gallery route
 app.get('/gallery', (req, res) => {
     console.log(__dirname);
     res.sendFile(`${__dirname}/views/gallery.html`)
